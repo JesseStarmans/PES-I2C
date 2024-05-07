eac3d552fe34a9a88907707ad6ea00243073094c
JesseStarmans <131788688+JesseStarmans@users.noreply.github.com>
2024-05-07 11:30:33 +0200

    Update SocketServer.h

--- Pi Socket/SocketServer.h
+++ Pi Socket/SocketServer.h
@@ -5,12 +5,15 @@ using namespace std;
 
 class SocketServer {
 public: 
-	SocketServer(int);
+	SocketServer(int, char*);
 	~SocketServer();
 	void listenForClients();
-	void sendData(const string&, int);
+	void sendData(const string&);
+	string receiveData();
 private:
 	int serverSocket;
 	sockaddr_in serverAddress;
+	int clientSocket;
 	int port;
+	char* IP;
 };
