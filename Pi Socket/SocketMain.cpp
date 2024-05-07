5286d5b9efffff4593c523a223e55b584b5a3fca
JesseStarmans <131788688+JesseStarmans@users.noreply.github.com>
2024-05-07 13:33:28 +0200

    Update SocketMain.cpp

--- Pi Socket/SocketMain.cpp
+++ Pi Socket/SocketMain.cpp
@@ -1,8 +1,20 @@
 #include "SocketServer.h"
+#include "SocketClient.h"
+
+#include <string>
 
 int main(void) {
 	char IP[16] = "145.52.127.169";
 	
 	SocketServer server(8080, IP);
+	
+	vector<string> IPsWemos = server.setupWemosIP();
+	SocketClient client(8080, IPsWemos[0].c_str());
+	
+	client.sendData("Test123");
+	string returning = client.receiveData();
+	
+	cout<<"In de main: "<<returning<<endl;
+	
 	server.listenForClients();
 }
