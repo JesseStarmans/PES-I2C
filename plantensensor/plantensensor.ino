2ee18aa325e2d2fa3af18a9aa265535d936f1c88
marijnvdwekken <50845116+marijnvdwekken@users.noreply.github.com>
2024-05-30 12:14:07 +0200

    Update plantensensor.ino

--- plantensensor/plantensensor.ino
+++ plantensensor/plantensensor.ino
@@ -1,63 +1,71 @@
-#include <ESP8266WiFi.h> // library voor de raspberry pi
-#include <WiFiClient.h>
-#include <avr/pgmspace.h>
-#include <LedControl.h>
-#include <string>
+#include <ESP8266WiFi.h>
+#include "WemosServer.h"
+#include "WemosClient.h"
 
-int rodepin= 14;
+int rodepin = 14;
 int groenepin = 12;
-int  blauwepin = 13;
+int blauwepin = 13;
 int waarde = 0;
 
+///SSID van het WiFi netwerk om mee te verbinden.
 const char* ssid = "NSELab";
+
+///Wachtwoord van het WiFi netwerk om mee te verbinden.
 const char* password = "NSELabWiFi";
-const char* serverIPaddress = "145.52.127.184"; // IP address van de Pi 
-const int port = 8080; // Port voor server
+
+///IP address van de Raspberry Pi server.
+const char* serverIPaddress = "145.52.127.184";
+
+///Port van de Raspberry Pi server.
+const int port = 8080;
+
+///Port voor eigen server.
 const int serverPort = 6060;
 
-const int numDevices = 4;      // number of MAX7219s used
-const long scrollDelay = 50;   // adjust scrolling speed
-unsigned long bufferLong [14] = {0};
+///Bool voor het versturen van de waarde
+bool sendValue = false; 
 
-LedControl lc = LedControl(12, 14, 15, 4); //din cs
+IPAddress getIPAddress();
 
+WemosServer server(serverPort);             /*!< Globale aanmaak van de WemosServer*/
+WemosClient client(getIPAddress(), port);   /*!< Globale aanmaak van de WemosClient*/
 
-WiFiServer server(serverPort);
-
-void setup() { 
+/*!
+  @brief De setup code die één keer wordt uitgevoerd.
+*/
+void setup() {
   Serial.begin(115200);
-   pinMode(rodepin, OUTPUT);
-  pinMode(groenepin, OUTPUT);
-  pinMode(blauwepin, OUTPUT);
-  pinMode(A0,INPUT);
-  //WiFi.begin(ssid, password);
 
-  //  while (WiFi.status() != WL_CONNECTED) {
-  //    delay(1000);
-  //    Serial.println("Connecting to WiFi...");
-  //  }
+  WiFi.begin(ssid, password);
 
-  Serial.println("Connected to WiFi");
-
-  // start TCP server
-  //server.begin();
-  Serial.println("Server started");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
-  
-  // sendIP functies bestaan om de server IP invulling te geven. Comment uit wat je niet gebruikt
-  // sendIP("Wemos1");
-  // sendIP("Wemos2");
-  // sendIP("Wemos3");
-  Serial.println("Waiting for request");
-   for (int x = 0; x < numDevices; x++) {
-    lc.shutdown(x, false);      //The MAX72XX is in power-saving mode on startup
-    lc.setIntensity(x, 8);      // Set the brightness to default value
-    lc.clearDisplay(x);         // and clear the display
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting to the WiFi");
+    delay(1000);
   }
+
+  Serial.println("Connected to the WiFi");
+
+  server.begin();
+  Serial.println("Server started");
+  Serial.println("IP: ");
+  Serial.println(WiFi.localIP());
+
+  
+  client.sendIP("Wemos1");
+  client.sendIP("Wemos2");
+  client.sendIP("Wemos3");
+
+  Serial.println("Waiting for request");
 }
 
-void sendIP(String name){
+/*!
+  @brief Maakt een automatisch een IPAddress object aan.
+
+  Deze functie maakt automatisch een IPAddress object aan, aan de hand van het serverIPaddress dat bovenaan gedefinieert staat.
+
+  @return Geeft het IPAddress object terug om te gebruiken in de WemosClient aanmaak.
+*/
+IPAddress getIPAddress() {
   int ipDelen[4];
   int i = 0;
   String tempString = serverIPaddress;
@@ -67,97 +75,24 @@ void sendIP(String name){
     ipDelen[i++] = bufferIP.toInt();
     tempString = tempString.substring(tempString.indexOf('.') + 1);
   }
-  IPAddress serverIP(ipDelen[0], ipDelen[1], ipDelen[2], ipDelen[3]);
-  
-  WiFiClient client;
-  if (client.connect(serverIP, port)){
-    Serial.println("Connected to server");
-  
-  // Send a message to the server
-  IPAddress localIP = WiFi.localIP();
-  char ipFull[50] = "";
-  sprintf(ipFull, "%s %d.%d.%d.%d", name, localIP[0], localIP[1],localIP[2],localIP[3]);
-  Serial.println(ipFull);
-  client.println(ipFull);
-  delay(150);
-  client.stop();
-  }
-  else {
-    Serial.println("Unable to connect to server");
-  }
+  return IPAddress(ipDelen[0], ipDelen[1], ipDelen[2], ipDelen[3]);
 }
 
+/*!
+  @brief De loop waarin de code herhaald blijft worden.
+*/
 void loop() {
-  //serverCode();
+  serverCode();
   ledaansturen();
-  //if (Serial.available() > 0) {
-    //String inlees = Serial.readStringUntil('\n');
-    //Serial.println("Ontvangen data: " + inlees);
-    //clientCodeMetSend(inlees);
-  //}
-}
-
-void serverCode(){
-  WiFiClient client = server.available();
-
-  if(client){
-    Serial.println("New Client");
-    while (client.connected()) {
-      if (client.available()) {
-        String request = client.readStringUntil('\r');
-        Serial.println("Request: " + request);
-
-        String weerTeGevenTekst;
-        if (request.indexOf("lichtkrant:") != -1) {
-          weerTeGevenTekst = request.substring(12);
-        }
-        
-        delay(100);
-
-        client.stop();
-        Serial.println("Client disconnected");
-    }
+  if (sendValue) {
+    clientCodeMetSend(String(waarde));
+    delay(10);
+    sendValue = false; 
   }
 }
-}
-
-void clientCode(){
-  if (WiFi.status() == WL_CONNECTED) {
-    int ipDelen[4];
-    int i = 0;
-    String tempString = serverIPaddress;
-    String bufferIP;
-    while (tempString.length() > 0) {
-      bufferIP = tempString.substring(0, tempString.indexOf('.'));
-      ipDelen[i++] = bufferIP.toInt();
-      tempString = tempString.substring(tempString.indexOf('.') + 1);
-    }
-    IPAddress serverIP(ipDelen[0], ipDelen[1], ipDelen[2], ipDelen[3]);
-  
-    WiFiClient client;
-    
-    if (client.connect(serverIP, port)) {
-      Serial.println("Connected to server");
-      
-      client.println("Hello from Wemos Lolin Mini!");
-      delay(150);
-
-      while (client.available() > 0) {
-        String response = client.readStringUntil('\r');
-        Serial.println(response);
-      }
-
-      client.stop();
-    } else {
-      Serial.println("Connection to server failed");
-    }
-    
-    delay(5000);
-  }
-}
-
 void clientCodeMetSend(String toSend) {
   if (WiFi.status() == WL_CONNECTED) {
+    // IP address of the server you want to connect to
     int ipDelen[4];
     int i = 0;
     String tempString = serverIPaddress;
@@ -170,37 +105,61 @@ void clientCodeMetSend(String toSend) {
     IPAddress serverIP(ipDelen[0], ipDelen[1], ipDelen[2], ipDelen[3]);
   
     WiFiClient client;
-    
     if (client.connect(serverIP, port)) {
       Serial.println("Connected to server");
       
+      // Send a message to the server
       client.println(toSend.c_str());
       delay(150);
 
+      // Close the connection
       client.stop();
     } else {
       Serial.println("Connection to server failed");
     }
     
-    delay(5000);
+    delay(1000);
   }
 }
-void setColor(int redValue, int greenValue,  int blueValue) {
+/*!
+  @brief Checkt of er een client verbinding is binnengekomen.
+
+  Deze functie checkt of er een nieuwe client verbinding is binnengekomen en leest dan de gestuurde string in.
+*/
+void serverCode() {
+  server.checkForConnection();
+
+  if (server.client) {
+    while (server.client.connected()) {
+      if (server.client.available()) {
+        String request = server.receiveRequest();
+        Serial.println("Request: " + request);
+        if(request == "210"){
+            sendValue = true; 
+        }
+
+      }
+    }
+  }
+}
+
+
+//led gedeelte
+void setColor(int redValue, int greenValue, int blueValue) {
   analogWrite(rodepin, redValue);
-  analogWrite(groenepin,  greenValue);
+  analogWrite(groenepin, greenValue);
   analogWrite(blauwepin, blueValue);
 }
-void ledaansturen(){
+
+void ledaansturen() {
   waarde = analogRead(A0);
-  Serial.println(waarde); 
-  if(waarde < 400){
+  //Serial.println(waarde);
+  if (waarde < 400) {
     setColor(255, 0, 0); // Red Color
+  } else if (waarde > 400 && waarde < 600) {
+    setColor(255, 128, 0); // oranje Color
+  } else if (waarde > 600 && waarde < 700) {
+    setColor(0, 255, 0); // groen Color
   }
-  else if(waarde > 400 && waarde < 600){
-    setColor(128, 128, 0); // Red Color
-  }
-  else if(waarde > 600 && waarde < 700){
-    setColor(0, 255, 0); // Red Color
-  }
- 
 }
+
