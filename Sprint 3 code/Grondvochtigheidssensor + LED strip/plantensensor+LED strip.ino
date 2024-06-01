d7d652d9c5cb6aac3b3e4b2fd97a2b51860e2567
marijnvdwekken <50845116+marijnvdwekken@users.noreply.github.com>
2024-06-01 20:25:04 +0200

    Update plantensensor+LED strip.ino
    Comments toegevoegd

--- Sprint 3 code/Grondvochtigheidssensor + LED strip/plantensensor+LED strip.ino
+++ Sprint 3 code/Grondvochtigheidssensor + LED strip/plantensensor+LED strip.ino
@@ -1,27 +1,30 @@
 #include <FastLED.h>
-#include <ESP8266WiFi.h> // library voor de raspberry pi
+#include <ESP8266WiFi.h> // library voor de ESP8266 microcontroller
 #include <WiFiClient.h>
 #include <avr/pgmspace.h>
 #include <LedControl.h>
 #include <string>
 
-#define LED_PIN     D4 // pin waar de led op aangesloten is
+// Definities van pinnen en instellingen
+#define LED_PIN     D4 // Pin waar de LED-strip op aangesloten is
 #define NUM_LEDS    20
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
-#define VERGROTING 10 // vergroting per helderheidsknop ingedrukt
-#define OMHOOG D2 // helderheid omhoog ledstrip
-#define OMLAAG D3 // helderheid omlaag ledstrip
-#define KLEURWISSEL D1 // ledstrip aan/uit
+#define VERGROTING 10 // Helderheidstoename per knopdruk
+#define OMHOOG D2 // Pin voor helderheid omhoog knop
+#define OMLAAG D3 // Pin voor helderheid omlaag knop
+#define KLEURWISSEL D1 // Pin voor kleurwissel knop
 #define UPDATES_PER_SECOND 100
 
+// WiFi-instellingen
 const char* ssid = "PiNetGroepG";
 const char* password = "GroepGNet";
-const char* serverIPaddress = "10.42.0.251"; // IP address van de Pi 
-const int port = 8080; // Port voor server
-const int serverPort = 6060;
-const unsigned long debounceDelay = 200;
+const char* serverIPaddress = "10.42.0.251"; // IP-adres van de server
+const int port = 8080; // Poort voor server
+const int serverPort = 6060; // Poort voor lokale server
+const unsigned long debounceDelay = 200; // Debounce vertraging voor knoppen
 
+// Definities voor RGB pins en andere variabelen
 int rodepin = 14;
 int groenepin = 12;
 int blauwepin = 13;
@@ -29,28 +32,31 @@ int waarde = 0;
 int helderheid = 128; 
 int colorIndex = 0; // Index voor de huidige kleur
 unsigned long lastButtonPress = 0;
-
 bool sendValue = false; 
 
+// Initialisatie van LED-strip en kleuren
 CRGB leds[NUM_LEDS];
 const CRGB HTMLColors[] = {
     CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Indigo, CRGB::Violet, CRGB::White, CRGB::Black
 };
 
+// Initialisatie van de WiFi server
 WiFiServer server(serverPort);
 
 void setup() {
-    Serial.begin(115200);
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-    FastLED.setBrightness(helderheid);
+    Serial.begin(115200); // Start seriële communicatie
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); // Initialiseer LED-strip
+    FastLED.setBrightness(helderheid); // Stel initiële helderheid in
     fill_solid(leds, NUM_LEDS, HTMLColors[colorIndex]);  
     FastLED.show();
     
+    // Stel pinnen in voor knoppen
     pinMode(OMHOOG, INPUT_PULLUP);
     pinMode(OMLAAG, INPUT_PULLUP);
     pinMode(KLEURWISSEL, INPUT_PULLUP);
-    WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password); // Verbind met WiFi
 
+    // Wacht tot de WiFi-verbinding tot stand is gebracht
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
         Serial.println("Connecting to WiFi...");
@@ -58,21 +64,22 @@ void setup() {
 
     Serial.println("Connected to WiFi");
 
-    // start TCP server
+    // Start de TCP-server
     server.begin();
     Serial.println("Server started");
     Serial.print("IP: ");
     Serial.println(WiFi.localIP());
     
-    // sendIP functies bestaan om de server IP invulling te geven. Comment uit wat je niet gebruikt
+    // Stuur IP-adressen van verschillende clients naar de server
     sendIP("Wemos1");
     sendIP("Wemos2");
     sendIP("Wemos3");
     Serial.println("Waiting for request");
-    waarde = analogRead(A0);
+    waarde = analogRead(A0); // Lees analoge waarde
 }
 
 void sendIP(String name) {
+    // Functie om IP-adres naar server te sturen
     int ipDelen[4];
     int i = 0;
     String tempString = serverIPaddress;
@@ -101,37 +108,40 @@ void sendIP(String name) {
 }
 
 void loop() { 
-  
-                    if (waarde < 400) {
-                        setColor(255, 0, 0); // Red Color
-                    } else if (waarde > 400 && waarde < 600) {
-                        setColor(255, 128, 0); // oranje Color
-                    } else if (waarde > 600 && waarde < 700) {
-                        setColor(0, 255, 0); // groen Color
-                    }
-    ledstripaansturen();
-     //ledaansturen();
-    serverCode();
+    // Controleer de waarde en stel de kleur in
+    if (waarde < 400) { //Plantensensor waarde
+        setColor(255, 0, 0); // Rode kleur
+    } else if (waarde > 400 && waarde < 600) { //Plantensensor waarde
+        setColor(255, 128, 0); // Oranje kleur
+    } else if (waarde > 600 && waarde < 700) { //Plantensensor waarde
+        setColor(0, 255, 0); // Groene kleur
+    }
+    ledstripaansturen(); // Aansturen van de ledstrip
+    serverCode(); // Afhandelen van server communicatie
       
+    // Herstel WiFi-verbinding indien verbroken
     if (WiFi.status() != WL_CONNECTED) {
         Serial.println("Reconnecting...");
         while (!WiFi.reconnect()) {}
     }
 
+    // Lees seriële invoer
     if (Serial.available() > 0) {
         String inlees = Serial.readStringUntil('\n');
         Serial.println("Ontvangen data: " + inlees);
         clientCodeMetSend(inlees);
     }
 
+    // Stuur waarde indien nodig
     if (sendValue) {
-        clientCodeMetSend(String(waarde));
+        clientCodeMetSend(String(waarde)); //Stuurt de waarde door naar de server
         delay(10);
         sendValue = false; 
     }
 }
 
 void serverCode() {
+    // Functie om inkomende client-verbindingen te verwerken
     WiFiClient client = server.available();
 
     if (client) {
@@ -142,9 +152,8 @@ void serverCode() {
                 Serial.println("Request: " + request);
 
                 if (request == "Plant") {
-                  waarde = analogRead(A0);
+                    waarde = analogRead(A0);
                     clientCodeMetSend(String(waarde));
-
                 }
                 
                 delay(100);
@@ -156,6 +165,7 @@ void serverCode() {
 }
 
 void clientCode() {
+    // Functie om verbinding met server te maken en gegevens te verzenden
     if (WiFi.status() == WL_CONNECTED) {
         int ipDelen[4];
         int i = 0;
@@ -191,6 +201,7 @@ void clientCode() {
 }
 
 void clientCodeMetSend(String toSend) {
+    // Functie om gegevens naar de server te verzenden
     if (WiFi.status() == WL_CONNECTED) {
         int ipDelen[4];
         int i = 0;
@@ -220,23 +231,20 @@ void clientCodeMetSend(String toSend) {
     }
 }
 
-// led gedeelte
+// Functie om kleur van de LEDs in te stellen
 void setColor(int redValue, int greenValue, int blueValue) {
-    analogWrite(rodepin, redValue);
-    analogWrite(groenepin, greenValue);
-    analogWrite(blauwepin, blueValue);
+    analogWrite(rodepin, redValue); //Schrijf de kleur naar de rodepin
+    analogWrite(groenepin, greenValue); //Schrijf de kleur naar de groenepin
+    analogWrite(blauwepin, blueValue); //Schrijf de kleur naar de oranjepin
 }
 
-// void ledaansturen() {
-  
-// }
-
+// Functie om LED-strip aan te sturen
 void ledstripaansturen() {
     unsigned long currentMillis = millis();
     if ((currentMillis - lastButtonPress) > debounceDelay) {
         if (digitalRead(OMHOOG) == LOW) {
             helderheid += VERGROTING;
-            if (helderheid > 255) { // zorg voor een randwaarde
+            if (helderheid > 255) { // Beperk helderheid tot maximaal 255
                 helderheid = 255;
             }
             FastLED.setBrightness(helderheid);
@@ -255,7 +263,7 @@ void ledstripaansturen() {
 
         if (digitalRead(OMLAAG) == LOW) {
             helderheid -= VERGROTING;
-            if (helderheid < 0) { // zorg voor een randwaarde
+            if (helderheid < 0) { // Beperk helderheid tot minimaal 0
                 helderheid = 0;
             }
             FastLED.setBrightness(helderheid);
