f6aa2537950976718effdf2f61536501ab1d2d0f
JiwiKiwi <114234262+JiwiKiwi@users.noreply.github.com>
2024-06-06 14:59:44 +0200

    Update PlantLED.ino
    zie comment in regel 1

--- LEDstrip&plantsensor/PlantLED.ino
+++ LEDstrip&plantsensor/PlantLED.ino
@@ -1,13 +1,15 @@
+//code voor ALLEEN de ledstrip, plant/grondsensor werkt hierbij nog niet. zie void servercode
+
 #include <FastLED.h>
-#include <ESP8266WiFi.h> // library voor de raspberry pi
+#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <avr/pgmspace.h>
 #include <LedControl.h>
 #include <string>
 
-#define LED_PIN     D8 // pin waar de led op aangesloten is
+#define LED_PIN     D0 // pin waar de led op aangesloten is
 #define NUM_LEDS    8
-#define LED_TYPE    WS2812B
+#define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
 #define VERGROTING 10 // vergroting per helderheidsknop ingedrukt
 #define OMHOOG D2 // helderheid omhoog ledstrip
@@ -15,12 +17,13 @@
 #define KLEURWISSEL D1 // ledstrip aan/uit
 #define UPDATES_PER_SECOND 100
 
-const char* ssid = "PiNetGroepG";
-const char* password = "GroepGNet";
-const char* serverIPaddress = "10.42.0.251"; // IP address van de Pi 
+const char* ssid = "NSELab";
+const char* password = "NSELabWiFi";
+const char* serverIPaddress = "145.52.127.184"; // IP address van de Pi 
 const int port = 8080; // Port voor server
 const int serverPort = 6060;
-const unsigned long debounceDelay = 200;
+const unsigned long debounceDelay = 300; // verhoog de debounce delay
+int status = 0;
 
 int rodepin = 14;
 int groenepin = 12;
@@ -29,9 +32,6 @@ int waarde = 0;
 int helderheid = 128; 
 int colorIndex = 0; // Index voor de huidige kleur
 unsigned long lastButtonPress = 0;
-int status = 0;
-
-bool sendValue = false; 
 
 CRGB leds[NUM_LEDS];
 const CRGB HTMLColors[] = {
@@ -53,8 +53,12 @@ void setup() {
     pinMode(OMHOOG, INPUT_PULLUP);
     pinMode(OMLAAG, INPUT_PULLUP);
     pinMode(KLEURWISSEL, INPUT_PULLUP);
-    WiFi.begin(ssid, password);
 
+    pinMode(rodepin, OUTPUT);
+    pinMode(groenepin, OUTPUT);
+    pinMode(blauwepin, OUTPUT);
+
+    WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
         Serial.println("Connecting to WiFi...");
@@ -67,14 +71,12 @@ void setup() {
     Serial.println("Server started");
     Serial.print("IP: ");
     Serial.println(WiFi.localIP());
-    
-    // sendIP functies bestaan om de server IP invulling te geven. Comment uit wat je niet gebruikt
+
     sendIP("Wemos1");
     sendIP("Wemos2");
     sendIP("Wemos3");
     sendIP("Wemos4");
     Serial.println("Waiting for request");
-    waarde = analogRead(A0);
 }
 
 void sendIP(String name) {
@@ -106,164 +108,83 @@ void sendIP(String name) {
 }
 
 void loop() { 
-  
-                    if (waarde < 400) {
-                        setColor(255, 0, 0); // Red Color
-                    } else if (waarde > 400 && waarde < 600) {
-                        setColor(255, 128, 0); // oranje Color
-                    } else if (waarde > 600 && waarde < 700) {
-                        setColor(0, 255, 0); // groen Color
-                    }
     ledstripaansturen();
-     //ledaansturen();
     serverCode();
-      
     if (WiFi.status() != WL_CONNECTED) {
         Serial.println("Reconnecting...");
         while (!WiFi.reconnect()) {}
     }
-
-    /*if (Serial.available() > 0) { //dit vindt de ledstrip niet leuk als je helderheid omhoog of omlaag wilt zetten.
-        String inlees = Serial.readStringUntil('\n');
-        Serial.println("Ontvangen data: " + inlees);
-        clientCodeMetSend(inlees);
-    }*/
-
-    if (sendValue) {
-        clientCodeMetSend(String(waarde));
-        delay(10);
-        sendValue = false; 
-    }
 }
 
-void serverCode() {
+void serverCode() { //servercode zonder de grondsensor code erbij toegevoegd.
     WiFiClient client = server.available();
     if (client) {
         Serial.println("New Client");
-        //while (client.connected()) {
-            if (client.available()) {
-                Serial.println("Test1");
-                String request = client.readStringUntil('\r');
-                Serial.println("Request: " + request);
-                if (request == "Plant") {
-                    waarde = analogRead(A0);
-                    if (waarde < 500) {
-                        setColor(255, 0, 0); // Red Color
-                        status = 1;
-                    } else if (waarde >=500) {
-                        
-                        setColor(0, 255, 0); // groen Color
-                        status = 2;
-                    }
-                    String deel1 = "Rpan: ";
-                    String deel2 = String(status);
-                    clientCodeMetSend(deel1 + deel2);
-                }
-                delay(100);
-                client.stop();
-                Serial.println("Client disconnected");
-            }
-        //}
-    }
-    Serial.println("Uit de functie servercode");
-}
-
-void clientCode() {
-    if (WiFi.status() == WL_CONNECTED) {
-        int ipDelen[4];
-        int i = 0;
-        String tempString = serverIPaddress;
-        String bufferIP;
-        while (tempString.length() > 0) {
-            bufferIP = tempString.substring(0, tempString.indexOf('.'));
-            ipDelen[i++] = bufferIP.toInt();
-            tempString = tempString.substring(tempString.indexOf('.') + 1);
+        String request = "";
+        while (client.connected() && client.available()) {
+            request += (char)client.read();
         }
-        IPAddress serverIP(ipDelen[0], ipDelen[1], ipDelen[2], ipDelen[3]);
-    
-        WiFiClient client;
-        
-        if (client.connect(serverIP, port)) {
-            Serial.println("Connected to server");
-            
-            client.println("Hello from Wemos Lolin Mini!");
-            delay(150);
-
-            while (client.available() > 0) {
-                String response = client.readStringUntil('\r');
-                Serial.println(response);
-            }
-
-            client.stop();
-        } else {
-            Serial.println("Connection to server failed");
+        if (request.length() > 0) {
+            Serial.println("Request: " + request);
         }
-        
-        delay(500);
+        client.stop();
+        Serial.println("Client disconnected");
     }
 }
 
-void clientCodeMetSend(String toSend) {
-    if (WiFi.status() == WL_CONNECTED) {
-        int ipDelen[4];
-        int i = 0;
-        String tempString = serverIPaddress;
-        String bufferIP;
-        while (tempString.length() > 0) {
-            bufferIP = tempString.substring(0, tempString.indexOf('.'));
-            ipDelen[i++] = bufferIP.toInt();
-            tempString = tempString.substring(tempString.indexOf('.') + 1);
-        }
-        IPAddress serverIP(ipDelen[0], ipDelen[1], ipDelen[2], ipDelen[3]);
-    
-        WiFiClient client;
-        
-        if (client.connect(serverIP, port)) {
-            Serial.println("Connected to server");
-            
-            client.println(toSend.c_str());
-            delay(150);
+// void serverCode() {
+//     WiFiClient client = server.available();
+//     if (client) {
+//         Serial.println("New Client");
+//         String request = "";
+//         while (client.connected() && client.available()) {
+//             request += (char)client.read();
+//         }
+//         if (request.length() > 0) {
+//             Serial.println("Request: " + request);
+//             if (request.indexOf("Plant") != -1) { // Use indexOf to avoid exact string matching issues
+//                 int waarde = analogRead(A0);
+//                 Serial.println("Waarde: " + String(waarde));
+//                 if (waarde < 500) {
+//                     setColor(255, 0, 0);
+//                 } else {
+//                     setColor(0, 255, 0);
+//                 }
+//                 String deel1 = "Rpan: ";
+//                 String deel2 = String(waarde < 500 ? 1 : 2);
+//                 client.println(deel1 + deel2);
+//             }
+//         }
+//         client.stop();
+//         Serial.println("Client disconnected");
+//     }
+// }
 
-            client.stop();
-        } else {
-            Serial.println("Connection to server failed");
-        }
-        
-        delay(500);
-    }
-}
-
-// led gedeelte
 void setColor(int redValue, int greenValue, int blueValue) {
     analogWrite(rodepin, redValue);
     analogWrite(groenepin, greenValue);
     analogWrite(blauwepin, blueValue);
 }
 
-// void ledaansturen() {
-  
-// }
-
 void ledstripaansturen() {
     unsigned long currentMillis = millis();
     if ((currentMillis - lastButtonPress) > debounceDelay) {
         if (digitalRead(OMHOOG) == LOW) {
             helderheid += VERGROTING;
-            if (helderheid > 255) { // zorg voor een randwaarde
+            if (helderheid > 255) {
                 helderheid = 255;
             }
             FastLED.setBrightness(helderheid);
-            FastLED.getBrightness();
-            Serial.println((int)FastLED.getBrightness());
             FastLED.show();
+            Serial.println("Helderheid omhoog: " + String((int)FastLED.getBrightness()));
             lastButtonPress = currentMillis;
         }
 
         if (digitalRead(KLEURWISSEL) == LOW) {
             colorIndex = (colorIndex + 1) % (sizeof(HTMLColors) / sizeof(HTMLColors[0]));
-            //for (int i = 0; i < 255; i++) {
-                fill_solid(leds, NUM_LEDS, HTMLColors[colorIndex]);
-                FastLED.show();
+            fill_solid(leds, NUM_LEDS, HTMLColors[colorIndex]);
+            Serial.println("Kleur wisselen: " + String(colorIndex));
+            FastLED.show();
             lastButtonPress = currentMillis;
         }
 
@@ -274,6 +195,7 @@ void ledstripaansturen() {
                 helderheid = 0;
             }
             FastLED.setBrightness(helderheid);
+            Serial.println("Helderheid omlaag: " + String((int)FastLED.getBrightness()));
             FastLED.show();
             lastButtonPress = currentMillis;
         }
